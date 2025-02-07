6c1c17151bbf390c03a1c1f829673acf6a5c3fe0
Ryan Succo <rsucco@live.com>
2025-02-07 15:17:09 -0500

    Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Requires `godot3-bin` to be on your path, can be easily installed with the godot3-bin AUR package
 
-godot3-bin --export "Linux/X11" ./builds/Flotilla.x86_64
-godot3-bin --export "Windows Desktop" ./builds/Flotilla.exe
-godot3-bin --export "Mac OSX" ./builds/Flotilla.zip
+godot3-bin --export "Linux/X11" ./builds/Flotilla_Linux.x86_64	
+godot3-bin --export "Windows Desktop" ./builds/Flotilla_Windows.exe
+godot3-bin --export "Mac OSX" ./builds/Flotilla_macOS.zip
 
